334a7ccfbd032b2193eafc49af6a3cb25b125987
soffiblanco <msblanco@unis.edu.gt>
2024-10-10 17:33:13 -0600

    navbar update

--- src/layouts/header/NavBar.jsx
+++ src/layouts/header/NavBar.jsx
@@ -3,6 +3,8 @@ import './NavBar.css';
 import { IoSearchSharp, IoMenuSharp } from "react-icons/io5";
 import { Link } from 'react-router-dom';
 import Paws from '../../assets/Paws.png';
+import { TiShoppingCart } from "react-icons/ti";
+import { FaUserPlus, FaUser } from "react-icons/fa";
 
 const Navbar = () => {
     const [isMenuOpen, setIsMenuOpen] = useState(false);
@@ -23,27 +25,31 @@ const Navbar = () => {
                 <IoMenuSharp />
             </div>
 
+            <div className='search-box'>
+                <input type="text" placeholder='Search' />
+                <IoSearchSharp className='search-icon' />
+            </div>
+
             <ul className={`nav-list ${isMenuOpen ? 'active' : ''}`}>
-                {['Home', 'Blog', 'About Us', 'Login', 'Sign Up'].map((item) => (
+                {['Home', 'Categories', 'About Us', 'Orders', 'Sign Up'].map((item) => (
                     <li key={item}
                         onClick={() => handleItemClick(item)}
-                        className={activeItem === item ? 'active' : ''}
-                    >
+                        className={activeItem === item ? 'active' : ''}>
                         <Link to={`/${item.toLowerCase().replace(' ', '-')}`} onClick={() => handleItemClick(item)}>
                             {item}
                         </Link>
                     </li>
                 ))}
+                <div className='icon-container'>
+                    <div className='search-icon'><TiShoppingCart size={30} /></div>
+                    <div className='search-icon'><FaUserPlus size={25} /></div>
+                    <div className='search-icon'><FaUser size={20} /></div>
+                </div>
             </ul>
 
-            <img src={Paws} alt='paws' className='Paws'/>
-
-            <div className='search-box'>
-                <input type="text" placeholder='Search' />
-                <IoSearchSharp className='search-icon' />
-            </div>
+            <img src={Paws} alt='paws' className='Paws' />
         </div>
     );
-}
+};
 
 export default Navbar;
